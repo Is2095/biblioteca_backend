7395f51e5321bc573fd34577ef718baa7a061c8c
Is2095 <ismael2095951@gmail.com>
2024-07-05 19:14:15 -0300

    resolución bug bcrypt.compare y database-password

--- src/controller/getUsuarioPorEmailBD.js
+++ src/controller/getUsuarioPorEmailBD.js
@@ -12,7 +12,7 @@ const BuscarUsuarioPorEmailBD = (req, res) => {
 
     const db = coneccionBD();
 
-    db.query('SELECT * FROM usuarios WHERE usuarios.email = ?', [email], (err, result) => {
+    db.query('SELECT * FROM usuarios WHERE usuarios.email = ?', [email], async (err, result) => {
         if (err) {
             desconeccionBD(db);
             res.status(404).json({ err: 'error al buscar el usuario' })
@@ -21,7 +21,7 @@ const BuscarUsuarioPorEmailBD = (req, res) => {
             if (result.length === 0) {
                 res.status(200).json({ err: "no se encontró el usuario" })
             } else {
-                const isValid = bcrypt.compare(password, result[0].password)
+                const isValid = await bcrypt.compare(password, result[0].password)
                 if (!isValid) {
                     res.status(404).json({ err: 'error de validación de usuario' })
                 } else {
